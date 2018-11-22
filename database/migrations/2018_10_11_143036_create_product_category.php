fa09b099992b49384e105658d0ff029c218b5794
andiramdani27 <andi8337@gmail.com>
2018-11-22 14:18:49 +0700

    update migration

--- database/migrations/2018_10_11_143036_create_product_category.php
+++ database/migrations/2018_10_11_143036_create_product_category.php
@@ -19,7 +19,7 @@ class CreateProductCategory extends Migration
             $table->integer('category_id')->unsigned();
 
             $table->foreign('product_id')->references('id')->on('products');
-            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
